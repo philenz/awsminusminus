7bab7016bba0ec4726520130b498b047c4323043
Phil Evans <phil.evans@gallagher.com>
2017-08-18 16:05:22 +1200

    Rename bucket so can set up DNS

--- deployS3.sh
+++ deployS3.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-aws s3 cp lib s3://awsplusplus/lib/ --recursive
+webbucket=awsplusplus.security.gallagher.io
 
-aws s3 cp index.html s3://awsplusplus/
-aws s3 cp apigClient.js s3://awsplusplus/
+aws s3 cp lib s3://$webbucket/lib/ --recursive
+
+aws s3 cp index.html s3://$webbucket/
+aws s3 cp apigClient.js s3://$webbucket/
 
 exit 0
