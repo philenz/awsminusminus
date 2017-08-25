74482df63f4aef9b23247f471434cab9b8c69b71
Phil Evans <phil.evans@gallagher.com>
2017-08-25 16:55:20 +1200

    Move to datatables

--- access_keys.js
+++ access_keys.js
@@ -8,18 +8,15 @@ $(document).ready(function() {
 
         $("#waitMessage").hide();
 
-        $.each(users.data["userskeys"], function() {
-
-            rowHTML = '<tr>';
-            rowHTML += '<td>' + this["id"] + '</td>';
-            rowHTML += '<td>' + this["name"] + '</td>';
-            rowHTML += '<td>' + this["key"] + '</td>';
-            rowHTML += '<td>' + this["key_status"] + '</td>';
-            rowHTML += '<td>' + this["key_age"] + '</td>';
-            rowHTML += '</tr>';
-
-            $('#accesskeys').append(rowHTML);
-
+        $('#accesskeys').DataTable({
+            "data": users.data["userskeys"],
+            "columns": [
+                { "data": "id" },
+                { "data": "name" },
+                { "data": "key" },
+                { "data": "key_status" },
+                { "data": "key_age" }
+            ]
         });
     });
 });
