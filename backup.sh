1489a1fd8c27c1ebe58981cd8f8ad023ad4813b0
Phil Evans <phil.evans@gallagher.com>
2017-08-30 12:45:43 +1200

    Create a backup of files listed to Google Drive

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Create a backup of files listed to Google Drive
+
+files=( 'etc/config.json' )
+
+skicka mkdir /Source/awsminusminus
+
+for file in $files
+do
+    echo Copying $file to Google Drive
+
+	fname=`basename $file`
+	dname=`dirname $file`
+
+	skicka mkdir -p /Source/awsminusminus/$dname
+	skicka upload $file /Source/awsminusminus/$dname/$fname
+done
+
+exit 0
