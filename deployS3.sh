33b09aa70d4da18e7b01f4eb4a48ca57d7bae2eb
Phil Evans <phil.evans@gallagher.com>
2017-08-30 12:28:27 +1200

    copy config directory

--- deployS3.sh
+++ deployS3.sh
@@ -4,6 +4,7 @@ webbucket=awsplusplus.security.gallagher.io
 
 aws s3 cp lib s3://$webbucket/lib/ --recursive
 aws s3 cp aws s3://$webbucket/aws/ --recursive
+aws s3 cp etc s3://$webbucket/etc/ --recursive
 
 for html in *.html
 do
