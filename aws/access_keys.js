3a901af93041e034eba5dd0bcc78a98f37dcaa56
Phil Evans <phil.evans@gallagher.com>
2017-09-03 18:43:23 +1200

    get idToken, api url from session storage

--- aws/access_keys.js
+++ aws/access_keys.js
@@ -1,65 +1,36 @@
 $(document).ready(function() {
 
-    var config = Config.getInstance();
+    $("#waitMessage").show();
 
-    // authenticate against cognito...
-    var poolData = {
-        UserPoolId: config.userPoolId,
-        ClientId: config.clientId
-    };
-    var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-    var userData = {
-        Username: config.userName,
-        Pool: userPool
-    };
-    var authenticationData = {
-        Username: config.userName,
-        Password: config.password
-    };
-    var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
-    var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+    var awsHeaders = {};
+    awsHeaders['Authorization'] = sessionStorage.idToken;
 
-    cognitoUser.authenticateUser(authenticationDetails, {
+    var url = sessionStorage.awsPlusPlus + '/userskeys';
 
-        onSuccess: function (result) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        headers: awsHeaders,
+        crossDomain: true,
+        contentType: 'application/json',
+        success: function (data) {
 
-            $("#waitMessage").show();
+            $("#waitMessage").hide();
 
-            var awsHeaders = {};
-            awsHeaders['Authorization'] = result.getIdToken().getJwtToken();
-
-            var url = config.awsPlusPlus + '/userskeys';
-
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'json',
-                headers: awsHeaders,
-                crossDomain: true,
-                contentType: 'application/json',
-                success: function (data) {
-
-                    $("#waitMessage").hide();
-
-                    $('#accesskeys').DataTable({
-                        "data": data["userskeys"],
-                        //"pageLength": 20,
-                        "columns": [
-                            {"data": "name"},
-                            {"data": "key"},
-                            {"data": "key_status"},
-                            {"data": "key_age"}
-                        ]
-                    });
-                },
-                error: function (e) {
-                    alert("Error getting users and keys: " + JSON.stringify(e));
-                }
+            $('#accesskeys').DataTable({
+                "data": data["userskeys"],
+                //"pageLength": 20,
+                "columns": [
+                    {"data": "name"},
+                    {"data": "key"},
+                    {"data": "key_status"},
+                    {"data": "key_age"}
+                ]
             });
         },
-        onFailure: function(e) {
-            alert("Error authenticating user: " + JSON.stringify(e));
+        error: function (e) {
+            alert("Error getting users and keys: " + JSON.stringify(e));
         }
     });
-
 });
--- aws/security_groups.js
+++ aws/security_groups.js
@@ -1,63 +1,35 @@
 $(document).ready(function() {
 
-    var config = Config.getInstance();
+    $("#waitMessage").show();
 
-    // authenticate against cognito...
-    var poolData = {
-        UserPoolId: config.userPoolId,
-        ClientId: config.clientId
-    };
-    var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-    var userData = {
-        Username: config.userName,
-        Pool: userPool
-    };
-    var authenticationData = {
-        Username: config.userName,
-        Password: config.password
-    };
-    var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
-    var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+    var awsHeaders = {};
+    awsHeaders['Authorization'] = sessionStorage.idToken;
 
-    cognitoUser.authenticateUser(authenticationDetails, {
+    var url = sessionStorage.awsPlusPlus + '/securitygroups';
 
-        onSuccess: function (result) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        headers: awsHeaders,
+        crossDomain: true,
+        contentType: 'application/json',
+        success: function (data) {
 
-            $("#waitMessage").show();
+            $("#waitMessage").hide();
 
-            var awsHeaders = {};
-            awsHeaders['Authorization'] = result.getIdToken().getJwtToken();
-
-            var url = config.awsPlusPlus + '/securitygroups';
-
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'json',
-                headers: awsHeaders,
-                crossDomain: true,
-                contentType: 'application/json',
-                success: function (data) {
-
-                    $("#waitMessage").hide();
-
-                    $('#securitygroups').DataTable({
-                        "data": data["security_groups"],
-                        //"pageLength": 20,
-                        "columns": [
-                            { "data": "name" },
-                            { "data": "vpc" },
-                            { "data": "description" }
-                        ]
-                    });
-                },
-                error: function (e) {
-                    alert("Error getting security groups: " + JSON.stringify(e));
-                }
+            $('#securitygroups').DataTable({
+                "data": data["security_groups"],
+                //"pageLength": 20,
+                "columns": [
+                    { "data": "name" },
+                    { "data": "vpc" },
+                    { "data": "description" }
+                ]
             });
         },
-        onFailure: function(e) {
-            alert("Error authenticating user: " + JSON.stringify(e));
+        error: function (e) {
+            alert("Error getting security groups: " + JSON.stringify(e));
         }
     });
 
